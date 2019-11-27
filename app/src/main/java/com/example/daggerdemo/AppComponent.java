1b571af657f16f382bc9676024713cbec5737e5c
Nagendra Chaudhary <nagendra.chaudhary@travelyaari.com>
2019-11-27 17:22:07 +0530

    kkkk

--- app/src/main/java/com/example/daggerdemo/AppComponent.java
+++ app/src/main/java/com/example/daggerdemo/AppComponent.java
@@ -2,7 +2,6 @@ package com.example.daggerdemo;
 
 import com.example.daggerdemo.carparts.ActivityComponent;
 import com.example.daggerdemo.carparts.driverparts.DriverModule;
-import com.example.daggerdemo.carparts.engineparts.DieselEngineModule;
 
 import javax.inject.Singleton;
 
